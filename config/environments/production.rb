8bdbbf1ccefa82de68acc48a39909a7745e5c10e
huyhuynh0296 <huyhuynhdtu@gmail.com>
2018-04-11 15:28:14 +0700

    Chapter_7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.force_ssl = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -8,7 +8,6 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
