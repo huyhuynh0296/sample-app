f7571f95dcb3c28ff8e4430ab2d28b071e3b22c1
huyhuynh0296 <huyhuynhdtu@gmail.com>
2018-04-11 13:55:41 +0700

    chapter_8
    chapter_7
    
    Chapter_7
    
    chapter_6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   before_save{self.email = email.downcase}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  has_secure_password
   validates :name, presence: true, length: {maximum: Settings.user.maximum_name}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.user.maximum_email},
   format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  has_secure_password
   validates :password, presence: true, length: {minimum: Settings.user.minimum_password}
+
+  def self.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20180411071216) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
